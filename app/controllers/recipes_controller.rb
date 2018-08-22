2f3574f1f19fc7691f2d0a645c1ccbf72d37d948
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-22 14:55:55 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "Ingredient 1")
+    @recipe.ingredients.build(name: "Ingredient 2")
+
   end
 
   def create
+    @recipe = Recipe.new(params.require(:recipe).permit(:title, ingredients_attributes: [:name,:quantity]))
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+
 end
